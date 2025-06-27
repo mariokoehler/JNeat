5915b7c973265334d0e05c0832558eb09d3743e1
Mario Köhler <mario.koehler@otto-payments.de>
2025-06-27 14:00:51 +0200

    wip

--- combine.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem -Recurse -Filter *.java | ForEach-Object {
-    "--- START FILE: $($_.FullName) ---"
-    Get-Content $_.FullName
-    "--- END FILE: $($_.FullName) ---`n`n"
-} | Set-Content -Encoding UTF8 combined_code.txt
